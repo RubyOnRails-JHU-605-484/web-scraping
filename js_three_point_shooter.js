d794562457b6365b3d58689025c99a3cd021b180
Kalman Hazins <kalmanh@gmail.com>
2019-12-01 10:40:19 -0500

    Initial commit

---
+++ js_three_point_shooter.js
@@ -0,0 +1,114 @@
+// npm install node-fetch cheerio cli-table
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const Table = require('cli-table');
+
+const addAveragePerYear = players => {
+  return players.map(player => {
+    const { yearsPlayed, threePointersMade } = player;
+    const numberOfYearsPlayed =
+      parseInt(yearsPlayed[1]) - parseInt(yearsPlayed) + 1;
+    player['averagePerYear'] = (
+      parseFloat(threePointersMade.replace(',', '')) / numberOfYearsPlayed
+    ).toFixed(2);
+    return player;
+  });
+};
+
+const sortByAveragePerYear = players =>
+  players.sort(
+    (player1, player2) => player2.averagePerYear - player1.averagePerYear
+  );
+
+const compact_begin_end_years = yearsStr => {
+  let result = yearsStr.replace(/present/g, new Date().getFullYear());
+  result = result.match(/\(\d.*?\d\)/g);
+
+  result = result.map(eachPair => {
+    eachPair = eachPair.replace(/\(|\)/g, '');
+    eachPair = eachPair.replace(/\u2013/g, ',');
+    eachPair = eachPair.split(',').map(str => parseInt(str));
+    return eachPair;
+  });
+
+  const flattenedSortedArray = [].concat(...result).sort();
+
+  return [
+    flattenedSortedArray[0],
+    flattenedSortedArray[flattenedSortedArray.length - 1]
+  ];
+};
+
+const fetchPlayers = async () => {
+  let players = [];
+  const response = await fetch(
+    'https://en.wikipedia.org/wiki/List_of_National_Basketball_Association_career_3-point_scoring_leaders'
+  );
+  const page = await response.text();
+  const $ = cheerio.load(page);
+  const tableRows = $('table.wikitable.sortable tr');
+
+  tableRows.each((i, elem) => {
+    const tds = $(elem).find('td');
+    if (tds.length > 0) {
+      players.push({
+        name: $(tds[1])
+          .text()
+          .trim(),
+        yearsPlayed: compact_begin_end_years(
+          $(tds[3])
+            .text()
+            .trim()
+        ),
+        threePointersMade: $(tds[4])
+          .text()
+          .trim(),
+        fieldGoalPercentage: $(tds[6])
+          .text()
+          .trim()
+      });
+    }
+  });
+
+  return players;
+};
+
+const TOP_NUMBER_OF_PLAYERS = 10;
+
+async function main() {
+  let players = await fetchPlayers();
+  players = addAveragePerYear(players);
+  sortByAveragePerYear(players);
+
+  const t = new Table({
+    head: [
+      'Name',
+      'Years Played'.padStart(13),
+      '3s made'.padStart(10),
+      'Avg / Year',
+      'FG %'
+    ]
+  });
+
+  players.slice(0, TOP_NUMBER_OF_PLAYERS).forEach(player => {
+    const {
+      name,
+      yearsPlayed,
+      fieldGoalPercentage,
+      threePointersMade,
+      averagePerYear
+    } = player;
+
+    t.push([
+      name.padEnd(20),
+      yearsPlayed.join(' - ').padStart(13),
+      threePointersMade.padStart(10),
+      averagePerYear.padStart(10),
+      fieldGoalPercentage
+    ]);
+  });
+
+  console.log(t.toString());
+}
+
+main();
---
+++ three_point_shooters.rb
@@ -0,0 +1,84 @@
+# Inspired by 
+# https://medium.freecodecamp.org/how-to-scrape-with-ruby-and-nokogiri-and-map-the-data-bd9febb5e18a
+
+require 'open-uri'
+
+# gem install nokogiri
+# gem install terminal-table
+require 'nokogiri'
+require 'terminal-table'
+
+def fetch_players
+  url = 'https://en.wikipedia.org/wiki/List_of_National_Basketball_Association_career_3-point_scoring_leaders'
+  html = open(url)
+  doc = Nokogiri::HTML(html)
+  players = []
+  table = doc.at('//*[@id="mw-content-text"]/div/table[2]')
+
+  table.search('tbody tr').each do |tr|
+    cells = tr.search('td')
+    next if cells.empty?
+
+    players.push(
+      name: cells[1].text,
+      years_played: compact_begin_end_years(cells[3].text), 
+      three_pointers_made: cells[4].text,
+      field_goal_percentage: cells[6].text
+    )
+  end
+
+  players
+end
+
+def compact_begin_end_years(years_str)
+  years_str.gsub!('present', Time.new.year.to_s)
+  result = years_str.scan(/\(\d.*?\d\)/)
+  result = result.map do |begin_end_year_pair|
+    begin_end_year_pair.gsub!(/\(|\)/, '')
+    begin_end_year_pair.gsub!("\u2013", ',')
+    begin_end_year_pair.split(",").map!(&:to_i)
+  end
+
+  result = result.flatten.sort
+  [result.first, result.last]
+end
+
+def add_average_per_year(players)
+  players.map do |player|
+    number_of_years_played = 
+      player[:years_played].last - player[:years_played].first + 1
+    player[:average_per_year] = 
+      player[:three_pointers_made].delete(',').to_f / number_of_years_played
+    player
+  end
+  players
+end
+
+def sort_by_average_per_year(players)
+  players.sort_by { |player| player[:average_per_year] }.reverse
+end
+
+def print_table(players)
+  headings = ["Name", "Years Played", "3s made", "Avg / Year", "FG %"]
+  table = Terminal::Table.new headings: headings do |t|
+    players.each do |player|
+      t.add_row [ 
+        player[:name], 
+        player[:years_played].join(' - '),
+        player[:three_pointers_made],
+        player[:average_per_year],
+        player[:field_goal_percentage]
+      ]  
+    end
+  end
+
+  2.upto(4) { |col| table.align_column(col, :right) }
+
+  puts table
+end
+
+NUMBER_OF_PLAYERS = 10
+players = fetch_players
+players = add_average_per_year(players)
+players = sort_by_average_per_year(players).first(NUMBER_OF_PLAYERS)
+print_table(players)
